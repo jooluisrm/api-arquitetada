fada7edf0b4d2fd259c37f9938b535cf957bb4f1
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2025-02-04 09:54:19 -0300

    Entendendo Services e como usar

---
+++ src/services/contact.ts
@@ -0,0 +1,13 @@
+import * as ContactModel from '../models/contact';
+
+export const getContacts = async () => {
+    const list = await ContactModel.getContacts();
+}
+
+export const createContact = async (name: string) => {
+    await ContactModel.createContact(name); 
+}
+
+export const deleteContact = async (name: string) => {
+    await ContactModel.deleteContact(name);
+}
